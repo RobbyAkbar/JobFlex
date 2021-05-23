343bd4472e3f508c00a33057fdfbdb5734272052
Robby-Akbar <robbyakbar@upi.edu>
2021-05-23 18:03:21 +0700

    add home and search result ui

--- app/src/main/java/com/exwara/jobflex/ui/main/MainActivity.kt
+++ app/src/main/java/com/exwara/jobflex/ui/main/MainActivity.kt
@@ -1,16 +1,34 @@
 package com.exwara.jobflex.ui.main
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.exwara.jobflex.R
+import com.exwara.jobflex.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        supportActionBar?.setDisplayShowTitleEnabled(false)
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        supportActionBar?.elevation = 0F
+        binding.toolbar.title = getString(R.string.job_flex)
+        setSupportActionBar(binding.toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_profile, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.action_profile) {
+            onBackPressed()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
